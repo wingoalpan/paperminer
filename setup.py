1b656e7c0ffec0da13c4004d501ef393eefda365
Wingoal <panwingoal@gmail.com>
2024-12-07 12:18:45 +0800

    Optimization: add post-install handle in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,52 @@
 #!C:\Users\pypy2\AppData\Local\Programs\Python\Python311\python.exe
 
 from __future__ import print_function
-from setuptools import setup, find_packages
+from setuptools import setup, Command
+from setuptools.command.install import install
+import os
+import shutil
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        move_config_db(self)
+
+
+def move_config_db(self):
+    user_dir = os.path.expanduser('~')
+    paperminer_dir = os.path.join(user_dir, '.paperminer')
+    if not os.path.exists(paperminer_dir):
+        os.mkdir(paperminer_dir)
+    # 安装配置文件config.json
+    config_file = os.path.join(paperminer_dir, 'config.json')
+    if not os.path.exists(config_file):
+        print('installing config.json ...')
+        shutil.move('paperminer/config.json', config_file)
+    # 安装配置文件database
+    db_file = os.path.join(paperminer_dir, 'db/papers.db')
+    if not os.path.exists(db_file):
+        db_dir = os.path.join(paperminer_dir, 'db')
+        if not os.path.exists(db_dir):
+            os.mkdir(db_dir)
+        print('installing db/papers.db ...')
+        shutil.move('paperminer/db/papers.db', db_file)
+    # 安装导出模版文件 template/*
+    template_dir = os.path.join(paperminer_dir, 'template')
+    if not os.path.exists(template_dir):
+        os.mkdir(template_dir)
+    template_files = os.listdir('paperminer/template')
+    for template_file in template_files:
+        target_file = os.path.join(template_dir, template_file)
+        if not os.path.exists(target_file):
+            print('installing template/{template_file} ...')
+            shutil.move(f'paperminer/template/{template_file}', target_file)
+    # 准备输出目录 output
+    output_dir = os.path.join(paperminer_dir, 'output')
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
 
 setup(
     name="paperminer",
@@ -11,12 +56,19 @@ setup(
     description="Paper lineage miner and a paper viewer.",
     license="MIT",
     url="https://github.com/wingoalpan/paperminer",  # github地址或其他地址
-    packages=['paperminer', 'test', 'paperminer.web', 'paperminer.web.pages'],
-    package_dir={'paperminer': 'paperminer', 'test': 'test',
+    packages=['paperminer', 'paperminer.db', 'paperminer.template', 'test',
+              'paperminer.web', 'paperminer.web.pages', 'paperminer.web.assets'],
+    package_dir={'paperminer': 'paperminer',
+                 'paperminer.db': 'paperminer/db',
+                 'paperminer.template': 'paperminer/template',
                  'paperminer.web': 'paperminer/web',
-                 'paperminer.web.pages': 'paperminer/web/pages'},
+                 'paperminer.web.pages': 'paperminer/web/pages',
+                 'paperminer.web.assets': 'paperminer/web/assets',
+                 'test': 'test'
+                 },
+    data_files=[],
     package_data={'': ['*.json', 'template/*.xlsx',
-                       'db/*.sql', 'db/*.db',
+                       'db/*.sql', 'db/papers.db',
                        'web/assets/*.css', 'web/assets/*.js']
                   },
     include_package_data=True,
@@ -26,11 +78,13 @@ setup(
             'dbsql=paperminer.dbsql:main',
         ]
     },
-
+    cmdclass={
+        'install': PostInstallCommand
+    },
     classifiers=[
         "Environment :: Windows Environment",
         'Intended Audience :: Academic Researchers or Technical Experts',
-        'License :: OSI Approved :: MIT License',
+        'License :: MIT License',
         'Natural Language :: Chinese',
         'Operating System :: Microsoft',
         'Operating System :: POSIX',
